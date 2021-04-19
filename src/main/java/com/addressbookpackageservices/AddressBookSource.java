fef22027b7baa7e421b3bac1eb2c450e435ea65b
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-19 23:40:51 +0530

    ADDED : ability edit using person name

--- src/main/java/com/addressbookpackageservices/AddressBookSource.java
+++ src/main/java/com/addressbookpackageservices/AddressBookSource.java
@@ -13,7 +13,7 @@ public class AddressBookSource {
     private String phoneNumber;
 
     Scanner scanner = new Scanner(System.in);
-    public String enterDetails(){
+    public void enterDetails(){
         System.out.println("ENTER THE DETAILS");
         System.out.println("ENTER THE FIRST NAME :");
         this.firstName= scanner.nextLine();
@@ -31,7 +31,67 @@ public class AddressBookSource {
         this.email= scanner.nextLine();
         System.out.println("ENTER THE PHONE NUMBER :");
         this.phoneNumber= scanner.nextLine();
-         return "\nAddressBookSource{" +
+    }
+
+    public  void editDetails(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What details you want to edit: ");
+        System.out.println("1 for firstname\n2 for last name\n3 for address\n4 for city\n5 for state\n6 for email\n7 for zip code\n8 for phone number\n9 for exit");
+        int choice = scanner.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println("first name to be edited to ");
+                String fName = input.nextLine();
+                this.firstName = fName;
+                break;
+
+            case 2:
+                System.out.println("last name to be edited to");
+                String lName = input.nextLine();
+                this.lastName = lName;
+                break;
+            case 3:
+                System.out.println("address to be edited to");
+                String newAddress = input.nextLine();
+                this.address = newAddress;
+                break;
+            case 4:
+                System.out.println("City to be edited to");
+                String newCity = input.nextLine();
+                this.city = newCity;
+                break;
+            case 5:
+                System.out.println("State to be edited to");
+                String newState = input.nextLine();
+                this.state = newState;
+                break;
+            case 6:
+                System.out.println("email to be edited to");
+                String newEmail = input.nextLine();
+                this.email = newEmail;
+                break;
+            case 7:
+                System.out.println("zip code to be edited to");
+                String newZip = input.nextLine();
+                this.email = newZip;
+                break;
+            case 8:
+                System.out.println("phone number to be edited to");
+                String newPhone = input.nextLine();
+                this.email = newPhone;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "AddressBookSource{" +
                 "\nfirstName='" + firstName + '\'' +
                 " \nLastName='" + lastName + '\'' +
                 " \naddress='" + address + '\'' +
@@ -43,17 +103,12 @@ public class AddressBookSource {
                 '}';
     }
 
-    @Override
-    public String toString() {
-        return "\nAddressBookSource{" +
-                "\nfirstName='" + firstName + '\'' +
-                " \nLastName='" + lastName + '\'' +
-                " \naddress='" + address + '\'' +
-                " \ncity='" + city + '\'' +
-                " \nstate='" + state + '\'' +
-                " \nzipCode='" + zipCode + '\'' +
-                " \nemail='" + email + '\'' +
-                " \nphoneNumber='" + phoneNumber + '\'' +
-                '}';
+    public String getFirstName() {
+        return firstName;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
 }
