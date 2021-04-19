1f517a57ada3597b4052d2ac720490717df51396
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-19 21:09:47 +0530

    ADDED : ability to create contact in address book

--- src/main/java/com/addressbookpackageservices/AddressBookRunner.java
+++ src/main/java/com/addressbookpackageservices/AddressBookRunner.java
@@ -1,7 +1,16 @@
 package com.addressbookpackageservices;
 
+import java.util.HashSet;
+
 public class AddressBookRunner {
+
+    static HashSet<AddressBookSource> personDetails = new HashSet<>();
+
     public static void main(String[] args) {
         System.out.println("WELCOME TO THE ADDRESS BOOK PROGRAM");
+        AddressBookSource addPerson = new AddressBookSource();
+        addPerson.enterDetails();
+        personDetails.add(addPerson);
+        System.out.println(personDetails);
     }
 }
